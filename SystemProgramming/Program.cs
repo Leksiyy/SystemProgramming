0bc032ac67b39adc11d2ed4f6705f8d284c975df
Leksiyy <leksiy197618@gmail.com>
2024-09-19 09:47:14 +0300

    first commit

---
+++ SystemProgramming/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SystemProgramming;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ParameterizedThreadStart threadStart = new ParameterizedThreadStart(Method2);
+        Thread thread = new Thread(Method1);
+        Thread thread2 = new Thread(Method2);
+        Console.ReadLine();
+    }
+
+    static void Method1()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine(i + "\n");
+        }
+    }
+
+    static void Method2(object o)
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine("\t\t\t" + o + "\n");
+        }
+    }
+}
