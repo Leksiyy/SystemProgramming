1ceb379c84869e9d1602be565341a656c52888c3
Leksiyy <leksiy197618@gmail.com>
2024-09-19 11:41:22 +0300

    first hw

---
+++ homework1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace homework1;
+
+class Program
+{
+    static void Main()
+    {
+        Process process = new Process();
+        
+        process.StartInfo.FileName = "/usr/bin/env";
+        process.StartInfo.Arguments = "bash";
+        process.Start();
+        
+        process.WaitForExit();
+        Console.WriteLine("Process ended");
+        
+        // на маке процессы уходят в фон и process.WaitForExit срабатывает сразу после запуска
+        // программы, поэтому вызвал утилиту
+    }
+}
