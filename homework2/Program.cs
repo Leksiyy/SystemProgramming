f2ba2df0f4b8bd8f21f6853fb36fa60dbc40226b
Leksiyy <leksiy197618@gmail.com>
2024-09-22 15:33:34 +0300

    first commit

---
+++ homework2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace homework2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread thread1 = new Thread(Room1);
+        Thread thread2 = new Thread(Room2);
+        
+        thread1.Start();
+        thread2.Start();
+    }
+
+    static void Room1()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Random rnd = new Random();
+            Console.WriteLine("Now room1 has {0} degrees", (15.0 + rnd.NextDouble() * (30.0 - 15.0)).ToString("F1"));
+            Thread.Sleep(1000);
+        }
+    }
+    
+    static void Room2()
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            Random rnd = new Random();
+            Console.WriteLine("\t\t\tNow room2 has {0} degrees", (15.0 + rnd.NextDouble() * (30.0 - 15.0)).ToString("F1"));
+            Thread.Sleep(800);
+        }
+    }
+}
