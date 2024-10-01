d8d29cf96e79b3e74fe49a1172e4af0fb1d5ad4a
Leksiyy <leksiy197618@gmail.com>
2024-10-01 12:27:34 +0300

    hw6

---
+++ homework6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace homework6;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string filePath = "../../../example.txt";
+
+        try
+        {
+            string content = await ReadFileAsync(filePath);
+
+            int characterCount = content.Length;
+
+            Console.WriteLine($"Количество символов в файле: {characterCount}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+
+    static async Task<string> ReadFileAsync(string filePath)
+    {
+        return await File.ReadAllTextAsync(filePath);
+    }
+}
